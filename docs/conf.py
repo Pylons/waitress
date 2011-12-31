313fe24b225833e07bc2b12c58b5714f2ea0a675
Chris McDonough <chrism@plope.com>
2011-12-31 05:58:25 -0500

    prep for 0.2

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ copyright = '2012, Agendaless Consulting <chrism@plope.com>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
