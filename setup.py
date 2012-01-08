e734fadcf293866e23446453bfa7dd4f4c499624
Chris McDonough <chrism@plope.com>
2012-01-08 17:53:34 -0500

    prep for 0.6.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except IOError:
 
 setup(
     name='waitress',
-    version='0.6',
+    version='0.6.1',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     maintainer="Chris McDonough",
