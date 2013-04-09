f2e4ce90290030f36e12cd2f595d4a60d0f6ad55
Tres Seaver <tseaver@palladion.com>
2013-04-09 10:46:01 -0400

    Make support for Python 3.3 explicit.

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         'Natural Language :: English',
