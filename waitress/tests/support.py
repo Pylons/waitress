725ddbded6b2a6d05bf38b9f551691019f77959c
Tres Seaver <tseaver@palladion.com>
2013-05-16 12:30:53 -0400

    Allow parallel testing (e.g., under 'detox').
    Use PID-dependent port / socket for functest servers.

--- waitress/tests/support.py
+++ waitress/tests/support.py
@@ -3,13 +3,30 @@ Support code for tests.
 """
 
 import logging
+import os
 import sys
 
 import waitress
 
+PID = os.getpid()
+port = os.environ.get('WAITRESS_TEST_PORT')
+if port is None:  # main process
+    # To permit parallel testing under 'detox', use a PID-dependent port:
+    # Subtract least-significant 20 bits of the PID from the top of the
+    # allowed port range
+    TEST_PORT = 0xffff - (PID & 0x7ff)
+    TEST_SOCKET_PATH = '/tmp/waitress.test-%d.sock' % PID
+    os.environ['WAITRESS_TEST_PORT'] = str(TEST_PORT)
+else:               # subprocess
+    TEST_PORT = int(port)
 
-TEST_PORT = 61523
-TEST_SOCKET_PATH = '/tmp/waitress.test.sock'
+socket = os.environ.get('WAITRESS_TEST_SOCKET')
+if socket is None:  # main process
+    # To permit parallel testing under 'detox', use a PID-dependent socket.
+    TEST_SOCKET_PATH = '/tmp/waitress.test-%d.sock' % PID
+    os.environ['WAITRESS_TEST_SOCKET'] = TEST_SOCKET_PATH
+else:               # subprocess
+    TEST_SOCKET_PATH = socket
 
 class NullHandler(logging.Handler):  # pragma: no cover
     """A logging handler that swallows all emitted messages."""
