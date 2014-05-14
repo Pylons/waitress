9ea10d51bd0a539a2ede91c220a917db438d1577
Bert JW Regeer <bertjw@regeer.org>
2014-05-14 12:57:33 -0600

    Replace magic numbers with errno.X
    This fixes the code so that running under OS X it raises
    ConnectionClosed as appropriate for when the connection is reset by
    peer.

--- waitress/tests/test_functional.py
+++ waitress/tests/test_functional.py
@@ -1473,7 +1473,8 @@ def read_http(fp): # pragma: no cover
     try:
         response_line = fp.readline()
     except socket.error as exc:
-        if get_errno(exc) in (10053, 10054, 104):
+        # errno 104 is ENOTRECOVERABLE, In WinSock 10054 is ECONNRESET
+        if get_errno(exc) in (errno.ECONNABORTED, errno.ECONNRESET, 104, 10054):
             raise ConnectionClosed
         raise
     if not response_line:
