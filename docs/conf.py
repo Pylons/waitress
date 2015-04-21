258a56462cc3196b9cae21c30dff324153c8f074
Steve Piercy <web@stevepiercy.com>
2015-04-21 14:10:28 -0700

    - add conf.py changes

--- docs/conf.py
+++ docs/conf.py
@@ -19,26 +19,7 @@
 
 import sys, os
 import pkg_resources
-
-# Add and use Pylons theme
-if 'sphinx-build' in ' '.join(sys.argv): # protect against dumb importers
-    from subprocess import call, Popen, PIPE
-
-    p = Popen('which git', shell=True, stdout=PIPE)
-    git = p.stdout.read().strip()
-    cwd = os.getcwd()
-    _themes = os.path.join(cwd, '_themes')
-
-    if not os.path.isdir(_themes):
-        call([git, 'clone', 'git://github.com/Pylons/pylons_sphinx_theme.git',
-                '_themes'])
-    else:
-        os.chdir(_themes)
-        call([git, 'checkout', 'master'])
-        call([git, 'pull'])
-        os.chdir(cwd)
-
-    sys.path.append(os.path.abspath('_themes'))
+import pylons_sphinx_themes
 
 # General configuration
 # ---------------------
@@ -109,8 +90,8 @@ pygments_style = 'sphinx'
 
 # Add and use Pylons theme
 sys.path.append(os.path.abspath('_themes'))
-html_theme_path = ['_themes']
 html_theme = 'pylons'
+html_theme_path = pylons_sphinx_themes.get_html_themes_path()
 html_theme_options = dict(github_url='http://github.com/Pylons/waitress')
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
