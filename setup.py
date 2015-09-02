3850e4c09aa9a55dc5c61985981b4ef719d5ebb8
Chris McDonough <chrism@plope.com>
2015-09-02 09:08:38 -0700

    prep for 0.8.10

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.version_info[:2] == (2, 6):
 
 setup(
     name='waitress',
-    version='0.8.10dev0',
+    version='0.8.10',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     maintainer="Chris McDonough",
