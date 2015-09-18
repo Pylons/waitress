a4f9cccc582138bca926d9c973a0a7cd13b4af87
Yu Zhou <nuaazy@163.com>
2015-09-18 11:06:56 +0800

    call prune to free occupied memory by output buffer

--- waitress/channel.py
+++ waitress/channel.py
@@ -249,6 +249,8 @@ class HTTPChannel(logging_dispatcher, object):
                             'Unexpected error when closing an outbuf')
                     continue # pragma: no cover (coverage bug, it is hit)
                 else:
+                    if hasattr(outbuf, 'prune'):
+                        outbuf.prune()
                     dobreak = True
 
             while outbuflen > 0:
