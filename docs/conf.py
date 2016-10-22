54e3ab8f7253b1c69d7c398a45154142a387d6ca
Steve Piercy <web@stevepiercy.com>
2016-10-22 12:12:27 -0400

    kill off one more instance of .static/logo_hi.gif

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the
 # top of the title page.
-latex_logo = '.static/logo_hi.gif'
+#latex_logo = '.static/logo_hi.gif'
 
 # For "manual" documents, if this is true, then toplevel headings are
 # parts, not chapters.
