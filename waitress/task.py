02ddee322bdc96f5f3670a15a7c53545c734bf7c
Jack Wearden <jack@jackwearden.co.uk>
2018-05-17 12:24:15 -0700

    Support configurably omitting server header

--- waitress/task.py
+++ waitress/task.py
@@ -250,10 +250,12 @@ class Task(object):
         # Set the Server and Date field, if not yet specified. This is needed
         # if the server is used as a proxy.
         ident = self.channel.server.adj.ident
-        if not server_header:
-            response_headers.append(('Server', ident))
-        else:
-            response_headers.append(('Via', ident))
+        if ident is not None:
+            if not server_header:
+                response_headers.append(('Server', ident))
+            else:
+                response_headers.append(('Via', ident))
+
         if not date_header:
             response_headers.append(('Date', build_http_date(self.start_time)))
 
