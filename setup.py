360909e2b5ac695356d50436284466b68543721b
Bert JW Regeer <bertjw@regeer.org>
2019-01-07 17:32:22 -0700

    Prep for v1.2.0b3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ testing_extras = [
 
 setup(
     name='waitress',
-    version='1.2.0b2',
+    version='1.2.0b3',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     maintainer="Pylons Project",
