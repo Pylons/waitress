bbc4db38d7d47a6f1d2e15d30a2722e085ecf972
Michael Merickel <michael@merickel.org>
2019-04-10 03:06:27 -0500

    demonstrate failing test with non-seekable filewrapper stream

--- waitress/tests/fixtureapps/filewrapper.py
+++ waitress/tests/fixtureapps/filewrapper.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 here = os.path.dirname(os.path.abspath(__file__))
@@ -12,6 +13,23 @@ class KindaFilelike(object): # pragma: no cover
         bytes = self.bytes[:n]
         self.bytes = self.bytes[n:]
         return bytes
+
+class UnseekableIOBase(io.RawIOBase): # pragma: no cover
+
+    def __init__(self, bytes):
+        self.buf = io.BytesIO(bytes)
+
+    def writable(self):
+        return False
+
+    def readable(self):
+        return True
+
+    def seekable(self):
+        return False
+
+    def read(self, n):
+        return self.buf.read(n)
 
 def app(environ, start_response): # pragma: no cover
     path_info = environ['PATH_INFO']
@@ -40,7 +58,8 @@ def app(environ, start_response): # pragma: no cover
                 ('Content-Type', 'image/jpeg'),
             ]
     else:
-        data = open(fn, 'rb').read()
+        with open(fn, 'rb') as fp:
+            data = fp.read()
         cl = len(data)
         f = KindaFilelike(data)
         if path_info == '/notfilelike':
@@ -48,6 +67,12 @@ def app(environ, start_response): # pragma: no cover
                 ('Content-Length', str(len(data))),
                 ('Content-Type', 'image/jpeg'),
             ]
+        elif path_info == '/notfilelike_iobase':
+            headers = [
+                ('Content-Length', str(len(data))),
+                ('Content-Type', 'image/jpeg'),
+            ]
+            f = UnseekableIOBase(data)
         elif path_info == '/notfilelike_nocl':
             headers = [('Content-Type', 'image/jpeg')]
         elif path_info == '/notfilelike_shortcl':
