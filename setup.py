cbc89bf742ef7cbca17671ec9acd3898491c378f
Bert JW Regeer <bertjw@regeer.org>
2020-02-02 21:50:27 -0800

    Prep v1.4.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ testing_extras = [
 
 setup(
     name="waitress",
-    version="1.4.2",
+    version="1.4.3",
     author="Zope Foundation and Contributors",
     author_email="zope-dev@zope.org",
     maintainer="Pylons Project",
