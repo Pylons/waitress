79ce1df6e426328df566ddc263e75850fb584bac
Keith Gaughan <k@stereochro.me>
2024-10-28 22:25:05 +0000

    Timeout functional tests
    If the functional tests don't exist after five seconds of waiting on
    them, they should be killed. This also uses pytest-timeout to impose a
    maximum runtime of 30secs on any test in the test suite.
    
    Hopefully this should prevent tests from hanging.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -97,7 +97,7 @@ class SubprocessTests:
             self.proc.terminate()
         self.sock.close()
         # This give us one FD back ...
-        self.proc.join()
+        self.proc.join(timeout=5)
         self.proc.close()
         self.queue.close()
         self.queue.join_thread()
@@ -163,9 +163,10 @@ class SleepyThreadTests(TcpTests, unittest.TestCase):
         time.sleep(3)
 
         for proc in procs:
+            proc.poll()
             if proc.returncode is not None:  # pragma: no cover
                 proc.terminate()
-            proc.wait()
+            proc.wait(timeout=5)
         # the notsleepy response should always be first returned (it sleeps
         # for 2 seconds, then returns; the notsleepy response should be
         # processed in the meantime)
